649fd853a61fdc3584c2905a8722b6818b37df42
S. C. Arzehd <scarzehd@gmail.com>
2024-02-26 22:40:51 -0500

    Added True Knife

--- src/main/java/com/scarzehd/otherworldldlyweapons/OtherworldlyWeapons.java
+++ src/main/java/com/scarzehd/otherworldldlyweapons/OtherworldlyWeapons.java
@@ -12,7 +12,6 @@ public class OtherworldlyWeapons implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Registering Mod Items");
 		ModItems.registerItems();
 	}
 }
--- src/main/java/com/scarzehd/otherworldldlyweapons/item/ModItems.java
+++ src/main/java/com/scarzehd/otherworldldlyweapons/item/ModItems.java
@@ -1,13 +1,24 @@
 package com.scarzehd.otherworldldlyweapons.item;
 
 import com.scarzehd.otherworldldlyweapons.OtherworldlyWeapons;
+import com.scarzehd.otherworldldlyweapons.item.custom.TrueKnifeSwordItem;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.item.ToolMaterials;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
-    public static void registerItems() { }
+    public static void registerItems() {
+        OtherworldlyWeapons.LOGGER.info("Registering Mod Items");
+    }
+
+    public static Item TEST_SWORD = registerItem("test_sword", new SwordItem(ToolMaterials.DIAMOND, 20, 2.5f, new FabricItemSettings().maxCount(1).maxDamage(1000)));
+
+    public static Item TRUE_KNIFE = registerItem("true_knife", new TrueKnifeSwordItem(ToolMaterials.DIAMOND, 1, 2, new FabricItemSettings().maxCount(1).maxDamage(1000)));
 
     public static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, new Identifier(OtherworldlyWeapons.MOD_ID, name), item);
