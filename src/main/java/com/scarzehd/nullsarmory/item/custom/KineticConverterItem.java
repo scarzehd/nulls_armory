c2c0cac127892238081e414f7fba66ae3ed0f383
S. C. Arzehd <scarzehd@gmail.com>
2024-03-15 19:38:21 -0400

    Added some bullshit

---
+++ src/main/java/com/scarzehd/nullsarmory/item/custom/KineticConverterItem.java
@@ -0,0 +1,34 @@
+package com.scarzehd.nullsarmory.item.custom;
+
+import com.google.common.collect.Maps;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.Multimaps;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.attribute.EntityAttribute;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class KineticConverterItem extends Item {
+
+    private static UUID uuid = UUID.fromString("9ebc77d6-5032-46f1-8f7b-1628e15d60ae");
+
+    public KineticConverterItem(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public Multimap<EntityAttribute, EntityAttributeModifier> getAttributeModifiers(ItemStack stack, EquipmentSlot slot) {
+        Multimap<EntityAttribute, EntityAttributeModifier> modifiers = Multimaps.newMultimap(Maps.newLinkedHashMap(), ArrayList::new);
+
+        if (slot == EquipmentSlot.OFFHAND) {
+            modifiers.put(EntityAttributes.GENERIC_ATTACK_DAMAGE, new EntityAttributeModifier(uuid, "convert_energy", 2, EntityAttributeModifier.Operation.ADDITION));
+        }
+
+        return modifiers;
+    }
+}
--- src/main/java/com/scarzehd/nullsarmory/item/custom/KineticConvertorItem.java
+++
@@ -1,9 +0,0 @@
-package com.scarzehd.nullsarmory.item.custom;
-
-import net.minecraft.item.Item;
-
-public class KineticConvertorItem extends Item {
-    public KineticConvertorItem(Settings settings) {
-        super(settings);
-    }
-}
