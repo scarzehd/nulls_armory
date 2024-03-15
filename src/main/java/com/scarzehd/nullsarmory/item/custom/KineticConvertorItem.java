d6413a2627673313d3760c7cc0d8bd46f5b848ef
Alex <Chromiumworldsmith@gmail.com>
2024-03-15 19:15:41 -0400

    added kinetic convertor go me

---
+++ src/main/java/com/scarzehd/nullsarmory/item/custom/KineticConvertorItem.java
@@ -0,0 +1,9 @@
+package com.scarzehd.nullsarmory.item.custom;
+
+import net.minecraft.item.Item;
+
+public class KineticConvertorItem extends Item {
+    public KineticConvertorItem(Settings settings) {
+        super(settings);
+    }
+}
