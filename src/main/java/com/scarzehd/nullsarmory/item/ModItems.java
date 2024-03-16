5a81c00f7d2b1ef63cc1935f8b6ac435a1713f03
Alex <Chromiumworldsmith@gmail.com>
2024-03-16 15:42:42 -0400

    eder ore block 2

--- src/main/java/com/scarzehd/nullsarmory/item/ModItems.java
+++ src/main/java/com/scarzehd/nullsarmory/item/ModItems.java
@@ -30,5 +30,5 @@ public class ModItems {
 
     public static final Item SHIELD_BELT = registerItem("shield_belt", new ShieldBeltItem(new FabricItemSettings().maxCount(1)));
 
-    public static final Item KINETIC_CONVERTER = registerItem("Kinetic_converter", new KineticConverterItem(new FabricItemSettings().maxCount(1)));
+    public static final Item KINETIC_CONVERTER = registerItem("kinetic_converter", new KineticConverterItem(new FabricItemSettings().maxCount(1)));
 }
