6738d272308f6e0641636d638899bf605ad00f15
S. C. Arzehd <scarzehd@gmail.com>
2024-03-15 23:19:54 -0400

    Moved stuff from PlayerEntityMixin to LivingEntityMixin Removed PlayerEntityMixin

--- src/main/java/com/scarzehd/nullsarmory/mixin/LivingEntityMixin.java
+++ src/main/java/com/scarzehd/nullsarmory/mixin/LivingEntityMixin.java
@@ -1,6 +1,9 @@
 package com.scarzehd.nullsarmory.mixin;
 
+import com.scarzehd.nullsarmory.NullsArmory;
 import com.scarzehd.nullsarmory.attribute.ModAttributes;
+import com.scarzehd.nullsarmory.components.IShieldsComponent;
+import com.scarzehd.nullsarmory.components.ModComponents;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
@@ -9,6 +12,7 @@ import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
@@ -24,5 +28,31 @@ abstract class LivingEntityMixin extends Entity {
         info.getReturnValue().add(ModAttributes.MAX_SHIELDS);
         info.getReturnValue().add(ModAttributes.SHIELDS_RECHARGE_DELAY);
         info.getReturnValue().add(ModAttributes.SHIELDS_RECHARGE_RATE);
+    }
+
+    @ModifyVariable(method = "applyDamage", at = @At("HEAD"))
+    private float modifyDamage(float amount) {
+        LivingEntity target = (LivingEntity)(Object)this;
+
+        IShieldsComponent shieldsComponent = ModComponents.SHIELDS.get(target);
+
+        double shields = shieldsComponent.getCurrentShields();
+
+        NullsArmory.LOGGER.info("Mixin");
+
+        if (shields > 0) {
+            double newShields = shields - amount;
+            if (newShields < 0) {
+                amount -= shields;
+                newShields = 0;
+            } else {
+                amount = 0;
+            }
+
+            shieldsComponent.setCurrentShields(newShields);
+            shieldsComponent.resetRechargeDelay();
+        }
+
+        return amount;
     }
 }
