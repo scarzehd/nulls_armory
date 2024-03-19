fd7492da2ae058b7aa4c4692540434ae80948b3d
S. C. Arzehd <scarzehd@gmail.com>
2024-03-19 19:30:05 -0400

    Added Shields HUD

--- src/main/java/com/scarzehd/nullsarmory/client/ShieldsHudOverlay.java
+++ src/main/java/com/scarzehd/nullsarmory/client/ShieldsHudOverlay.java
@@ -1,15 +1,77 @@
 package com.scarzehd.nullsarmory.client;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import com.scarzehd.nullsarmory.NullsArmory;
+import com.scarzehd.nullsarmory.attribute.ModAttributes;
+import com.scarzehd.nullsarmory.components.ModComponents;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.render.GameRenderer;
+import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.Identifier;
 
 public class ShieldsHudOverlay implements HudRenderCallback {
     public static final Identifier SHIELD_HEARTS_TEXTURE = new Identifier(NullsArmory.MOD_ID, "textures/gui/shield_hearts.png");
 
     @Override
-    public void onHudRender(DrawContext drawContext, float tickDelta) {
+    public void onHudRender(DrawContext context, float tickDelta) {
+        MinecraftClient client = MinecraftClient.getInstance();
 
+        if (client.options.hudHidden) return;
+        if (!client.interactionManager.hasStatusBars()) return;
+
+        PlayerEntity player = client.player;
+
+        int width = context.getScaledWindowWidth();
+        int height = context.getScaledWindowHeight();
+
+        int x;
+
+        int renderedHealth = (int)Math.ceil((player.getAttributeValue(EntityAttributes.GENERIC_MAX_HEALTH) + player.getAbsorptionAmount()) / 10);
+
+        int y;
+        int baseY = height - 29 - renderedHealth * 10;
+
+        if (player.getAttributeValue(EntityAttributes.GENERIC_ARMOR) > 0) {
+            baseY -= 10;
+        }
+
+        int maxShields = (int)Math.ceil(player.getAttributeValue(ModAttributes.MAX_SHIELDS)) / 2;
+
+        double shields = Math.ceil(ModComponents.SHIELDS.get(player).getCurrentShields());
+
+        RenderSystem.disableDepthTest();
+        RenderSystem.enableBlend();
+
+        for (int i = 0; i < maxShields; i++) {
+            x = width / 2 - 91 + (i % 10) * 8;
+            y = baseY - (int) (double) (i / 10) * 10;
+            drawShieldsContainer(context, x, y);
+        }
+
+        for (int i = 0; i < Math.floor(shields / 2); i++) {
+            x = width / 2 - 91 + (i % 10) * 8;
+            y = baseY - (int) (double) (i / 10) * 10;
+            drawShieldsHeart(context, x, y, false);
+        }
+
+        if (shields % 2 == 1) {
+            x = width / 2 - 91 + (((int)shields / 2) % 10) * 8;
+            y = baseY - (int)Math.floor(shields / 20) * 10;
+            drawShieldsHeart(context, x, y, true);
+        }
+
+        RenderSystem.disableBlend();
+        RenderSystem.enableDepthTest();
+    }
+
+    private void drawShieldsContainer(DrawContext context, int x, int y) {
+        context.drawTexture(SHIELD_HEARTS_TEXTURE, x, y, 18, 0, 9, 9, 27, 9);
+    }
+
+    private void drawShieldsHeart(DrawContext context, int x, int y, boolean half) {
+        context.drawTexture(SHIELD_HEARTS_TEXTURE, x, y, half ? 9 : 0, 0, 9, 9, 27, 9);
     }
 }
--- src/main/java/com/scarzehd/nullsarmory/mixin/PlayerEntityMixin.java
+++ src/main/java/com/scarzehd/nullsarmory/mixin/PlayerEntityMixin.java
@@ -1,33 +1,15 @@
 package com.scarzehd.nullsarmory.mixin;
 
-import com.scarzehd.nullsarmory.NullsArmory;
-import com.scarzehd.nullsarmory.attribute.ModAttributes;
 import com.scarzehd.nullsarmory.components.IShieldsComponent;
 import com.scarzehd.nullsarmory.components.ModComponents;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.attribute.DefaultAttributeContainer;
-import net.minecraft.world.World;
+import net.minecraft.entity.player.PlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(LivingEntity.class)
-abstract class LivingEntityMixin extends Entity {
-    LivingEntityMixin(final EntityType<?> type, final World world) {
-        super(type, world);
-    }
-
-    @Inject(method = "createLivingAttributes()Lnet/minecraft/entity/attribute/DefaultAttributeContainer$Builder;", at = @At("RETURN"))
-    private static void addAttributes(final CallbackInfoReturnable<DefaultAttributeContainer.Builder> info) {
-        info.getReturnValue().add(ModAttributes.MAX_SHIELDS);
-        info.getReturnValue().add(ModAttributes.SHIELDS_RECHARGE_DELAY);
-        info.getReturnValue().add(ModAttributes.SHIELDS_RECHARGE_RATE);
-    }
-
+@Mixin(PlayerEntity.class)
+public class PlayerEntityMixin {
     @ModifyVariable(method = "applyDamage", at = @At("HEAD"))
     private float modifyDamage(float amount) {
         LivingEntity target = (LivingEntity)(Object)this;
