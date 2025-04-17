7bd2e7f5d74d1692da0bccb77fa9f5beb26838ae
S. C. Arzehd <scarzehd@gmail.com>
2025-04-17 15:13:36 -0400

    Increased Yoshimitsu's Banner damage and range Changed Yoshimitsu's Banner model Added Yoshimitsu's Banner attack sound Fixed Yoshimitsu's Banner max stack size

--- src/main/java/com/scarzehd/nullsarmory/item/custom/YoshimitsuBannerItem.java
+++ src/main/java/com/scarzehd/nullsarmory/item/custom/YoshimitsuBannerItem.java
@@ -9,8 +9,10 @@ import net.minecraft.item.Item;
 public class YoshimitsuBannerItem extends Item {
     public YoshimitsuBannerItem() {
         super(new Item.Settings().attributeModifiers(AttributeModifiersComponent.builder()
-                .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, new EntityAttributeModifier(BASE_ATTACK_DAMAGE_MODIFIER_ID, 5f, EntityAttributeModifier.Operation.ADD_VALUE), AttributeModifierSlot.MAINHAND)
-                .add(EntityAttributes.GENERIC_ATTACK_SPEED, new EntityAttributeModifier(BASE_ATTACK_SPEED_MODIFIER_ID, -2.4F, EntityAttributeModifier.Operation.ADD_VALUE), AttributeModifierSlot.MAINHAND)
-                .build()));
+                    .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, new EntityAttributeModifier(BASE_ATTACK_DAMAGE_MODIFIER_ID, 6f, EntityAttributeModifier.Operation.ADD_VALUE), AttributeModifierSlot.MAINHAND)
+                    .add(EntityAttributes.GENERIC_ATTACK_SPEED, new EntityAttributeModifier(BASE_ATTACK_SPEED_MODIFIER_ID, -2.4F, EntityAttributeModifier.Operation.ADD_VALUE), AttributeModifierSlot.MAINHAND)
+                    .build())
+                .maxCount(1)
+        );
     }
 }
